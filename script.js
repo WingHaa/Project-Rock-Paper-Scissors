3c02abe8249df72980546225c6ddc9ecc0a28b63
WingHaa <quanlm24@gmail.com>
2022-03-26 02:11:16 -0700

    Added function to play 5 rounds; input prompt for Player and reworked winning logic

--- script.js
+++ script.js
@@ -1,64 +1,73 @@
 // User input their move
-// let playerSelection = prompt('Make your move!', '');
-let playerSelection = 'ROCK';
 // Computer make its move
-function randomFromOneToThree (max) {
-    return Math.floor(Math.random()*max + 1)
+function showPrompt () {
+    return prompt('Pick between "Rock", "Paper" or "Scissors"','')
 }
 
-let computerChoose = randomFromOneToThree(3);
+function formatPlayerInput () {
+    let playerInput = showPrompt()
+    return playerInput.trim().charAt(0).toUpperCase() + playerInput.trim().substring(1).toLowerCase()
+}
+
+function playerPlay () {
+    return input = formatPlayerInput()
+}
+
+function randomOneToThree () {
+    return Math.floor(Math.random()*3 + 1)
+}
 
 function computerPlay () {
+    let computerChoose = randomOneToThree();
     if (computerChoose == 1) return 'Rock'
     if (computerChoose == 2) return 'Paper'
     return 'Scissors'
 }
 
-let computerSelection = computerPlay();
 // Compute outcome
-let playerScore = 0;
-let computerScore = 0;
-function roundScore () {
-    if (playerSelection === computerSelection.toUpperCase()) return `It\'s a draw! We both drew ${computerSelection}`
 
-    if (playerSelection === 'ROCK') {
-        if (computerSelection === 'Paper') {
-            computerScore = ++computerScore;
-            return 'You Lose! Paper beats Rock'
-        }
-        if (computerSelection === 'Scissors') {
-            playerScore = ++playerScore;
-            return 'You Win! Rock beats Scissors'
-        }
+function playRound () {
+    let playerSelection = playerPlay();
+    let computerSelection = computerPlay();
+
+    console.log(playerSelection);
+    console.log(computerSelection);
+    if (playerSelection === computerSelection) {
+        return `It\'s a draw! We both drew ${computerSelection}`
     }
-
-    if (playerSelection === 'PAPER') {
-        if (computerSelection === 'Scissors') {
-            computerScore = ++computerScore;
-            return 'You Lose! Scissors beats Paper'
+    
+    let playerWinCondition = false;
+    if ((playerSelection === 'Rock' && computerSelection === 'Scissors') || 
+        (playerSelection === 'Paper' && computerSelection === 'Rock') || 
+        (playerSelection === 'Scissors' && computerSelection === 'Paper')) {
+            playerWinCondition = true;
         }
-        if (computerSelection === 'Rock') {
-            playerScore = ++playerScore;
-            return 'You Win! Paper beats Rock'
-        }     
-    }
-
-    if (playerSelection === 'SCISSORS') {
-        if (computerSelection === 'Rock') {
-            computerScore = ++computerScore;
-            return 'You Lose! Rock beats Scissors'
-        }
-        if (computerSelection === 'Paper') {
-            playerScore = ++playerScore;
-            return 'You Win! Scissors beats Paper'
-        }      
+    if (playerWinCondition) {
+        playerScore = ++playerScore;
+        return `You Win! ${playerSelection} beats ${computerSelection}`
+    } else{
+        computerScore = ++computerScore;
+        return `You Lost! ${computerSelection} beats ${playerSelection}`
     }
 }
+// Make the game last 5 rounds
 
-let result = roundScore();
+function game() {
+    for (let i = 0; i < 5; i++) {
+        if (playerScore == 3 || computerScore == 3) {break;}
+        let a = playRound();
+        console.log(a);
+    }
+    if (playerScore > computerScore) return `Congratulation! You won! The score is ${playerScore}:${computerScore}`
+    if (playerScore < computerScore) return `Too bad! You lost! The score is ${playerScore}:${computerScore}`
+    return `It's a tie! The score is ${playerScore}:${computerScore}`
+}
 
+let playerScore = 0;
+let computerScore = 0;
+let result = game();
 console.log(playerScore);
 console.log(computerScore);
 console.log(result);
-// Make the game last 5 rounds
 // Announce the winner
+
